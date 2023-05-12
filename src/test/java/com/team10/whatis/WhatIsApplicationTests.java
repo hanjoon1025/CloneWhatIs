5dc12dc460890db4cebd73c20bbfde559e7130fe
sojk401 <sojk401@gmail.com>
2023-05-12 21:21:00 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/com/team10/whatis/WhatIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team10.whatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
