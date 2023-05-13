1846b1d55b9c63efb636a0efe4bc5d084d86cff8
최한준 <hanjoon991@gmail.com>
2023-05-13 14:23:22 +0900

    ci/cd 테스트용 푸쉬 14번째 deploy.sh 수정-1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,46 @@
+##!/usr/bin/env bash
+#
+#REPOSITORY=/home/ubuntu/Clone_What_Is
+#cd $REPOSITORY
+#
+#APP_NAME=Clone_What_Is
+#JAR_NAME=$(find . -name "*.jar" | sort | tail -n 1)
+#JAR_PATH=./$JAR_NAME
+#CURRENT_PID=$(pgrep -f $APP_NAME)
+#
+#if [ -z "$CURRENT_PID" ]; then
+#echo "> 종료할 것 없음."
+#else
+#echo "> kill -15 $CURRENT_PID"
+#kill -15 $CURRENT_PID
+#sleep 5
+#fi
+#
+#echo "> $JAR_PATH 배포"
+#nohup java -jar $JAR_PATH > output.log 2>&1 &
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/Clone_What_Is
-cd $REPOSITORY
+BUILD_PATH=$(ls /home/ubuntu/Clone_What_Is/build/*.jar)
+JAR_NAME=$(basename "$BUILD_PATH")
+echo "> 빌드 파일명: $JAR_NAME"
 
-APP_NAME=Clone_What_Is
-JAR_NAME=$(find . -name "*.jar" | sort | tail -n 1)
-JAR_PATH=./$JAR_NAME
-CURRENT_PID=$(pgrep -f $APP_NAME)
+DEPLOY_PATH=/home/ubuntu/Clone_What_Is/
+echo "> 빌드 파일 복사"
+cp "$BUILD_PATH" "$DEPLOY_PATH"
+
+APPLICATION_JAR_NAME=Clone_What_Is.jar
+APPLICATION_JAR="$DEPLOY_PATH$APPLICATION_JAR_NAME"
+
+echo "> 현재 실행 중인 애플리케이션 PID 확인"
+CURRENT_PID=$(pgrep -f "$APPLICATION_JAR_NAME")
 
 if [ -z "$CURRENT_PID" ]; then
-echo "> 종료할 것 없음."
+  echo "> 종료할 애플리케이션이 없습니다."
 else
-echo "> kill -15 $CURRENT_PID"
-kill -15 $CURRENT_PID
-sleep 5
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
+  sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > output.log 2>&1 &
+echo "> $APPLICATION_JAR 배포"
+nohup java -jar "$APPLICATION_JAR" > output.log 2>&1 &
