089a5f39a042984857d5f8df5615eb9474187073
seokscirbe <seokscribe1970@gmail.com>
2023-05-13 18:57:02 +0900

    Feat: EmailController 메일발송 추가

---
+++ src/main/java/com/team10/whatis/email/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.team10.whatis.email.controller;
+
+import com.team10.whatis.email.dto.EmailRequestDto;
+import com.team10.whatis.email.entity.Email;
+import com.team10.whatis.email.service.EmailService;
+import com.team10.whatis.global.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/email")
+@RestController
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/auth")
+    @ResponseBody
+    public ResponseDto mailConfirm(@RequestBody EmailRequestDto emailRequestDto) throws Exception {
+        String code = emailService.sendSimpleMessage(emailRequestDto);
+        log.info("인증코드 : " + code);
+        return ResponseDto.setSuccess(null);
+    }
+}
