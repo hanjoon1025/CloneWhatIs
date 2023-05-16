5526b61a72d8844a68edc31832be93a9817e0787
joshWPark <parkjoshua98@gmail.com>
2023-05-16 19:41:53 +0900

    Feat : 회원가입 비밀번호 패턴 확인 추가

--- src/main/java/com/team10/whatis/member/dto/MemberRequestDto.java
+++ src/main/java/com/team10/whatis/member/dto/MemberRequestDto.java
@@ -1,5 +1,6 @@
 package com.team10.whatis.member.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ public class MemberRequestDto {
 
     private String email;
     private String username;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@!%*?&()_])[A-Za-z\\d$@!%*?&()_]{8,15}$",message = "비밀번호는 최소 8자 이상 15자 이하이며 알파벳 대소문자, 숫자와 특수문자로 구성되어야 합니다.")
     private String password;
     private String passwordCheck;
 
